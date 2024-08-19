7d4e1b444187cc6b293ecb197019cfd2bd56c5ce
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-08-19 18:19:41 +0530

    fix/minore changes

--- src/pages/product-overview.jsx
+++ src/pages/product-overview.jsx
@@ -40,7 +40,7 @@ export default function Example() {
     <div className="bg-gray-50">
       <header className="relative bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Product Overview</h1>
+          {/* <h1 className="text-3xl font-bold text-gray-900">Product Overview</h1> */}
         </div>
       </header>
 
